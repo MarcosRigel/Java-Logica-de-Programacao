eb2c8fcb042178f2f7cee6910f6c97c1f17aa731
Marcos Rigel <marcosrigel99@gmail.com>
2021-04-10 22:55:00 -0300

    Exercicios URI

--- URI-1013/src/Main.java
+++ URI-1013/src/Main.java
@@ -1,26 +1,3 @@
-/* O Maio
- * Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
- * MaiorAB = (a+b+abs(a-b)) / 2
- * 
- * Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
- * 
- * Entrada
- * O arquivo de entrada contém três valores inteiros.
- * 
- * Saida
- * Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
- * 
- * Exemplos:
- * ---------------------------------------
- * Entrada					Saida
- * ---------------------------------------
- * 7 14 106					106 eh o maior
- * ---------------------------------------
- * 217 14 6					217 eh o maior
- * 
- */
-
-
 import java.util.Scanner;
 
 public class Main {
--- URI-1017/src/Main.java
+++ URI-1017/src/Main.java
@@ -1,30 +1,3 @@
-/* Gasto de Combustível
- * Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa. 
- * Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). 
- * Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
- * 
- * Entrada
- * O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem (em horas) e o segundo é a velocidade média durante a mesma (em km/h).
- * 
- * Saida
- * Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal
- * 
- * Exemplos:
- * -------------------------------------
- * Entrada						Saida
- * -------------------------------------
- * 10							70.833
- * 85
- * -------------------------------------
- * 2							15.333
- * 92
- * --------------------------------------
- * 22							122.833
- * 67
- * 
- */
-
-
 import java.util.Locale;
 import java.util.Scanner;
 
