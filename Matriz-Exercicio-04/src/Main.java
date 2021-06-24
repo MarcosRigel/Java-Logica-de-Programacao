25a0e912e769bf18a61ae9fa19ed28e9fecb9edc
Marcos Rigel <>
2021-06-24 18:47:07 -0300

    Exercicio Matriz

---
+++ Matriz-Exercicio-04/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[][] mat = new int[N][N];
+        int soma = 0;
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        
+        for (int i = 0; i < N; i++) {
+                soma = soma + mat[i][i];
+            }
+            System.out.println(soma);
+        
+        sc.close();
+    }
+    
+}
