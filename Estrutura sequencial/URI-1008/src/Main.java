947b854924ca287e6d34697d658b6af317680415
Marcos Rigel <marcosrigel99@gmail.com>
2022-02-13 18:19:35 -0300

    Salario

--- Estrutura sequencial/URI-1008/src/Main.java
+++ Estrutura sequencial/URI-1008/src/Main.java
@@ -1,9 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int numeroFuncionario = sc.nextInt();
+		int numeroHoras = sc.nextInt();
+		double valorHoras = sc.nextDouble();
+		
+		double salario = numeroHoras * valorHoras;
+		
+		System.out.println("NUMBER = " + numeroFuncionario);
+		System.out.printf("SALARY = U$ %.2f%n", salario);
+		
+		sc.close();
 	}
-
 }
