7eb48cb18293facf644dc382291a2a5335986590
Marcos Rigel <marcosrigel99@gmail.com>
2022-02-13 18:10:45 -0300

    Soma Simples

--- Estrutura sequencial/URI-1003/src/Main.java
+++ Estrutura sequencial/URI-1003/src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int soma = a + b;
+		
+		System.out.println("SOMA = " + soma);
+		
+		sc.close();
+
 
 	}
 
