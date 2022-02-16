4338bd054a1ed40406b9f8cb96dc12a2d0a7aabd
Marcos Rigel <marcosrigel99@gmail.com>
2022-02-16 15:38:11 -0300

    Exercicio resolvido 02

---
+++ Estruturas repetitivas/Exemplo-03/src/Main.java
@@ -0,0 +1,49 @@
+/* Faça um programa para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo.
+ * O último dado, que não entrará nos cálculos, contém um valor de idade negativa. Calcular e imprimir a idade média
+ * deste grupo de indivíduos. Se for entrado um valor negativo na primeira vez, mostrar a mensagem "impossivel calcular".
+ * 
+ * Exemplos:
+ * ====================================================
+ * Entrada							Saída
+ * ====================================================
+ * 31								34.67
+ * 27
+ * 46
+ * -5
+ * ====================================================
+ * -10								impossivel calcular
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int idade = sc.nextInt();
+		int soma = 0;
+		int cont = 0;
+		
+		while (idade >= 0) {
+			soma = soma + idade;
+			cont = cont + 1;
+			idade = sc.nextInt();
+		}
+		
+		if (cont > 0) {
+			double media = (double) soma / cont;
+			System.out.printf("%.2f%n", media);
+		}
+		else {
+			System.out.println("impossivel calcular");
+		}
+		
+		sc.close();
+
+	}
+
+}
