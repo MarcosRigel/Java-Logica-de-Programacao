d7c4b62cebcb83e25a9002503604580683a9253d
Marcos Rigel <marcosrigel99@gmail.com>
2022-02-19 11:10:22 -0300

    Quadrado e ao cubo

--- Estruturas repetitivas/URI-1143/src/Main.java
+++ Estruturas repetitivas/URI-1143/src/Main.java
@@ -1,8 +1,27 @@
+import java.io.Console;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+        int primeiro = 0;
+        int segundo = 0;
+        int terceiro = 0;
+
+        for (int i = 1; i <= N; i++)
+        {
+            primeiro = i;
+            System.out.print(primeiro + " ");
+            segundo = primeiro * primeiro;
+            System.out.print(segundo + " ");
+            terceiro = segundo * primeiro;
+            System.out.println(terceiro);
+        }
+        
+        sc.close();
 
 	}
 
