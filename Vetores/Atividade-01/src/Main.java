7eb4ee440828001f9e8d1f725b12ce7e4eaf3fb0
Marcos Rigel <marcosrigel99@gmail.com>
2022-02-20 02:31:49 -0300

    maior numero e sua posicao

--- Vetores/Atividade-01/src/Main.java
+++ Vetores/Atividade-01/src/Main.java
@@ -1,4 +1,4 @@
-/* Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela o maio número
+/* Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela o maior número
  * do vetor (supor não haver empates). Mostrar também a posição do maior elemento
  * 
  * Exemplo
